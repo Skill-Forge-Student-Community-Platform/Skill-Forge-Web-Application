8b21eaf80fa8aa5baa57bd17203dd30c1be937b6
lakshanFernando2003 <lakshanchanaka34@gmail.com>
2025-02-27 15:40:34 +0530

    Add Achievement Center components and styles; update routing and configuration

---
+++ Front-End/Skill-Forge-WebSite/src/components/Achievementcenter/QuickStats.jsx
@@ -0,0 +1,14 @@
+// QuickStats.jsx (Quick Statistics)
+import React from "react";
+import "./QuickStats.css";
+
+const QuickStats = () => {
+  return (
+    <div className="quick-stats">
+      <h3>Quick Stats</h3>
+      <p>Competitions Won: 24</p>
+    </div>
+  );
+};
+
+export default QuickStats;
