97b92b4c938986ac55af0611fed6dfe7b4181f8c
mrdck2003 <dulitha.chathumina@gmail.com>
2025-02-28 11:01:53 +0530

    Authentication route and signup page

---
+++ Front-End/Skill-Forge-WebSite/src/components/Registration/Authentications.route.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Login from './Login.page';
+import SignUp from './SignUp.page';
+
+function Authentications() {
+  return (
+    <div className='min-h-screen bg-gradient-to-br from-gray-900 via-violet-400 via-blue-500 to-sky-600 flex items-center justify-center relative overflow-hidden'>
+      <Routes>
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default Authentications;
