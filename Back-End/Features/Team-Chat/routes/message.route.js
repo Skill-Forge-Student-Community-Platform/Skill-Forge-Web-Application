9d8ce11304aaf6e2550b5bb55401421fd8e994bf
amaneth20230365 <amaneth.20230365@iit.ac.lk>
2025-03-11 10:05:52 +0530

    message route is intergrated

--- Back-End/Features/Team-Chat/routes/message.route.js
+++ Back-End/Features/Team-Chat/routes/message.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {protectRoute} from "../middleware/auth.middleware.js"
-import { getMessages, getUsersForSidebar, sendMessages } from "../controllers/message.controller.js";
+import { getMessages, getUsersForSidebar, sendMessages } from "../Controllers/message.controller.js";
 
 
 const router = express.Router();
 
-
+// before gettiing the users the the route must be protected
+// when accessing this endpoint, its checks, whether the route is protected or not
+// if protected it then gets the users
 router.get("/users", protectRoute, getUsersForSidebar);
 // fetches messages with the user id
 router.get("/:id", protectRoute, getMessages);
