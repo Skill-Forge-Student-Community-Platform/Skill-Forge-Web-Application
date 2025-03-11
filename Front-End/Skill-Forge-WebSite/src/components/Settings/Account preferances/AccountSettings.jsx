aebabfbde7269df45512762e395ba57cbe6b4024
mrdck2003 <dulitha.chathumina@gmail.com>
2025-03-11 10:03:17 +0530

    add new code line for better readability in AccountSettings component

--- Front-End/Skill-Forge-WebSite/src/components/Settings/Account preferances/AccountSettings.jsx
+++ Front-End/Skill-Forge-WebSite/src/components/Settings/Account preferances/AccountSettings.jsx
@@ -63,6 +63,7 @@ export default function AccountSettings() {
       <SettingsSection {...profileSection} />
       <SettingsSection {...displaySection} />
       <SettingsSection {...preferencesSection} />
+      
     </div>
   )
 }
