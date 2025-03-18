2f3678570076edb3374b7534d7a8e7cf20be0182
Chaniru2003 <cmanmitha09@gmail.com>
2025-03-18 14:44:48 +0530

    sheduled files

---
+++ Back-End/Features/Team-Chat/Controllers/message.controller.js
@@ -0,0 +1,69 @@
+import User from "../models/user.model.js"
+import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
+
+// this route is being protected
+export const getUsersForSidebar = async(req, res)=>{
+     try {
+      // grabs the user id from this request
+        const loggedInUserId = req.user._id;
+
+      //   find all the users except current user and -
+      // -this fetches evering except tha password
+        const filteredUsers = await User.find({_id:{$ne:loggedInUserId}}).select("-password");
+
+        res.status(200).json(filteredUsers)
+     } catch (error) {
+        console.log("Error in getUsersForSidebar: ", error.message);
+        res.status(500).json({message:"Internal Server Error"});
+     }
+}
+
+export const getMessages = async(req, res)=>{
+   try {
+      const { id: userToChatId } = req.params
+      const myId = req.user._id
+
+      const message = await Message.find({
+         $or:[
+            {senderId:myId, receiverId:userToChatId},
+            {senderId:userToChatId, receiverId:myId},
+         ]
+      })
+      res.status(200).json(message);
+   } catch (error) {
+      console.log("Error in getMessage controller:",error.message);
+      res.status(500).json({message:"Internal Server Error"});
+   }
+}
+
+export const sendMessages = async(req, res)=>{
+   try {
+      const { text, image } = req.body;
+      const { id: receiverId } = req.params;
+      const senderId = req.user._id;
+      
+      let imageUrl;
+      if(image){
+         // upload base64 image to cloudinary
+         const uploadResponse = await cloudinary.uploader.upload(image);
+         imageUrl = uploadResponse.secure_url;
+      }
+
+      const newMessage = new Message({
+         senderId,
+         receiverId,
+         text,
+         image:imageUrl,
+      });
+
+      await newMessage.save();
+
+      // todo: real time functionality goes here via socket.io
+
+      res.status(201).json(newMessage)
+   } catch (error) {
+      console.log("Erro in sendMessage controller: ", error.message);
+      res.status(500).json({message:"Internal Sever Error"});
+   }
+} 
---
+++ Back-End/Features/Team-Chat/routes/message.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {protectRoute} from "../middleware/auth.middleware.js"
+import { getMessages, getUsersForSidebar, sendMessages } from "../controllers/message.controller.js";
+
+
+const router = express.Router();
+
+
+router.get("/users", protectRoute, getUsersForSidebar);
+// fetches messages with the user id
+router.get("/:id", protectRoute, getMessages);
+
+// Send message to the user id
+router.post("/send/:id", protectRoute, sendMessages);
+
+export default router;
