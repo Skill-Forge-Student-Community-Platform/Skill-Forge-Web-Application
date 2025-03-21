04b82bdd4839d73c7100aa08a73e81b24aaac8e9
Hasindu <pabasarahasindu9@gmail.com>
2025-03-21 14:00:15 +0530

    feat: add endpoint to fetch registered events for a user; integrate event details retrieval

--- Back-End/Features/EventRegister/routes/registerRoutes.js
+++ Back-End/Features/EventRegister/routes/registerRoutes.js
@@ -1,12 +1,13 @@
 
 
 import express from "express";
-import{ registerUser,getRegisteredUsersByEvent,removeUserFromEvent,getRegisteredUser } from "../controllers/registerController.js";
+import{ registerUser,getRegisteredUsersByEvent,removeUserFromEvent,getRegisteredUser,getRegisteredEventList } from "../controllers/registerController.js";
 
 const router = express.Router();
 // Register User Route
 router.post("/register", registerUser);
 router.get("/registered-users", getRegisteredUser);
+router.get("/registered-events-by-user/:userId", getRegisteredEventList);
 
 router.get("/registered-users/:eventId", getRegisteredUsersByEvent);
 
