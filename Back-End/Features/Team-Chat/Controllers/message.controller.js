6584997c81060b3b78d0cdb4633cca0566c4ac9e
amaneth20230365 <amaneth.20230365@iit.ac.lk>
2025-03-27 22:44:06 +0530

    Refactor team routes and user search store for improved readability and maintainability; enhance message controller error handling

--- Back-End/Features/Team-Chat/Controllers/message.controller.js
+++ Back-End/Features/Team-Chat/Controllers/message.controller.js
@@ -1,70 +1,74 @@
-import { User } from "../../User-Authentication/models/User.js"
+import { User } from "../../User-Authentication/models/User.js";
 import Message from "../models/message.model.js";
 import { v2 as cloudinary } from "cloudinary";
 
 // this route is being protected
-export const getUsersForSidebar = async(req, res)=>{
-     try {
-      // grabs the user id from this request
-        const loggedInUserId = req.user.id;
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    // grabs the user id from this request
+    const loggedInUserId = req.user.id;
 
-      //   find all the users except current user and -
-      // -this fetches evering except tha password
-        const filteredUsers = await User.find({_id:{$ne:loggedInUserId}}).select("-password");
+    //   find all the users except current user and -
+    // -this fetches evering except tha password
 
-        res.status(200).json(filteredUsers)
-     } catch (error) {
-        console.log("Error in getUsersForSidebar: ", error.message);
-        res.status(500).json({message:"Internal Server Error"});
-     }
-}
+    // with this, in the sidebar the current user will not be shown
+    // the current user will see his own username
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
 
-export const getMessages = async(req, res)=>{
-   try {
-      const { id: userToChatId } = req.params
-      const myId = req.user.id
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-      const message = await Message.find({
-         $or:[
-            {senderId:myId, receiverId:userToChatId},
-            {senderId:userToChatId, receiverId:myId},
-         ]
-      })
-      res.status(200).json(message);
-   } catch (error) {
-      console.log("Error in getMessage controller:",error.message);
-      res.status(500).json({message:"Internal Server Error"});
-   }
-}
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user.id;
 
-export const sendMessages = async(req, res)=>{
-   try {
-      const { text, image } = req.body;
-      const { id: receiverId } = req.params;
-      const senderId = req.user.id;
+    const message = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+    res.status(200).json(message);
+  } catch (error) {
+    console.log("Error in getMessage controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-      let imageUrl;
-      if(image){
-         // upload base64 image to cloudinary
-         const uploadResponse = await cloudinary.uploader.upload(image);
-         imageUrl = uploadResponse.secure_url;
-      }
+export const sendMessages = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user.id;
 
-      const newMessage = new Message({
-         senderId,
-         receiverId,
-         text,
-         image:imageUrl,
-      });
+    let imageUrl;
+    if (image) {
+      // upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
 
-      await newMessage.save();
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
 
-      // todo: real time functionality goes here via socket.io
+    await newMessage.save();
 
-      res.status(201).json(newMessage)
-   } catch (error) {
-      console.log("Erro in sendMessage controller: ", error.message);
-      res.status(500).json({message:"Internal Sever Error"});
-   }
-}
+    // todo: real time functionality goes here via socket.io
 
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Erro in sendMessage controller: ", error.message);
+    res.status(500).json({ message: "Internal Sever Error" });
+  }
+};
--- Back-End/Features/Team-collaboration/routes/team.route.js
+++ Back-End/Features/Team-collaboration/routes/team.route.js
@@ -1,20 +1,28 @@
-import express from 'express'
-import { verifyToken } from '../../User-Authentication/middleware/auth.middleware.js';
-import { createTeam, getTeamsByUser, sendInvite, respondToInvite, getReceivedInvites, getSentInvites, kickMemberFromTeam, getTeamsByTechnology } from '../Controllers/team.controller.js';
+import express from "express";
+import { verifyToken } from "../../User-Authentication/middleware/auth.middleware.js";
+import {
+  createTeam,
+  getTeamsByUser,
+  sendInvite,
+  respondToInvite,
+  getReceivedInvites,
+  getSentInvites,
+  kickMemberFromTeam,
+  getTeamsByTechnology,
+} from "../Controllers/team.controller.js";
 import { searchUsers } from "../Controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/createTeam", verifyToken, createTeam);
 router.get("/getTeams", verifyToken, getTeamsByUser);
-router.post('/invite', verifyToken, sendInvite);
-router.post('/respondToInvite', verifyToken, respondToInvite);
-router.get('/invites/received', verifyToken, getReceivedInvites);
-router.get('/invites/sent', verifyToken, getSentInvites);
-router.post('/kick-member', verifyToken, kickMemberFromTeam);
-router.get('/getTeamsByTechnology', verifyToken, getTeamsByTechnology);
+router.post("/invite", verifyToken, sendInvite);
+router.post("/respondToInvite", verifyToken, respondToInvite);
+router.get("/invites/received", verifyToken, getReceivedInvites);
+router.get("/invites/sent", verifyToken, getSentInvites);
+router.post("/kick-member", verifyToken, kickMemberFromTeam);
+router.get("/getTeamsByTechnology", verifyToken, getTeamsByTechnology);
 
-router.get("/search",verifyToken, searchUsers);
-
+router.get("/search", verifyToken, searchUsers);
 
 export default router;
