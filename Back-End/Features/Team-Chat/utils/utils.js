1f88daf52c4f34691257fcc9d7deb4b2186d89b4
lakshanFernando2003 <lakshanchanaka34@gmail.com>
2025-03-28 00:22:50 +0530

    Refactor cookie options in generateToken function for improved readability

--- Back-End/Features/Team-Chat/utils/utils.js
+++ Back-End/Features/Team-Chat/utils/utils.js
@@ -6,11 +6,11 @@ export const generateToken=(userId, res)=>{
     });
 
     res.cookie("jwt", token, {
-        maxAge: 7*24*60*60*1000, // MS
-        httpOnly: true, // prevents XSS attacks
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // CSRF protection
-        secure: process.env.NODE_ENV === "production", // Secure in production
-        domain: process.env.NODE_ENV === "production" ? process.env.COOKIE_DOMAIN : undefined
+      maxAge: 7*24*60*60*1000,
+      httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: process.env.NODE_ENV === "production",
+      domain: process.env.NODE_ENV === "production" ? process.env.COOKIE_DOMAIN : undefined
     });
 
     return token;
